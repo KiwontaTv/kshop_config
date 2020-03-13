5ead5ef28b7d079046ce07e1854426a35bc6b0fb
KiwontaTv <43320909+KiwontaTv@users.noreply.github.com>
2020-03-13 21:20:00 +0100

    Update config_kshop.lua

--- lua/autorun/config_kshop.lua
+++ lua/autorun/config_kshop.lua
@@ -1,31 +1,36 @@
-// The command tp add a shop
-KShop.AddShopCommand = "addshop" //Dont add a ! or /.
+local function LoadConfig()
+	// The command tp add a shop
+	KShop.AddShopCommand = "shop_addshop" //Dont add a ! or /.
 
-// The command to copy a shop
-KShop.CopyShopCommand = "copyshop" //Dont add a ! or /.
+	// The command to copy a shop
+	KShop.CopyShopCommand = "copyshop" //Dont add a ! or /.
 
-// The command to manage the shopsystem
-KShop.AdminShopCommand = "adminshop" //Dont add a ! or /.
+	// The command to manage the shopsystem
+	KShop.AdminShopCommand = "adminshop" //Dont add a ! or /.
 
-// Every ULX rank, which is able to edid/add/remove shops
-KShop.Admins = {"superadmin", "supervisor", "owner"}
+	// Every ULX rank, which is able to edid/add/remove shops
+	KShop.Admins = {"superadmin", "supervisor", "owner"}
 
-// Maximum armor, a player can buy. (e.g. a Player has 80 Armor and buys a package of 25 armor. If this is set to 100 it will never)
-// go higher that 100.
-KShop.MaxArmor = 100
+	// Maximum armor, a player can buy. (e.g. a Player has 80 Armor and buys a package of 25 armor. If this is set to 100 it will never)
+	// go higher that 100.
+	KShop.MaxArmor = 100
 
-// Color themes. Be careful :)
-KShop.UIColor = Color(69, 170, 242)
-KShop.Themes = {
-	primary = Color(83, 92, 104),
-	secondary = Color(123, 132, 144),
-	dark_underline = Color(53, 72, 74),
-	buttons = Color(99, 110, 114),
-	sel_but = Color(79, 90, 94),
+	// Color themes. Be careful :)
+	KShop.UIColor = Color(69, 170, 242)
+	KShop.Themes = {
+		primary = Color(83, 92, 104),
+		secondary = Color(123, 132, 144),
+		dark_underline = Color(53, 72, 74),
+		buttons = Color(99, 110, 114),
+		sel_but = Color(79, 90, 94),
 
-	light_red = Color(250, 157, 140),
-	dark_red = Color(214, 48, 49),
+		light_red = Color(250, 157, 140),
+		dark_red = Color(214, 48, 49),
 
-	buypanel = Color(180,190,195),
-	dark_one = Color(150,160,165),
-}
+		buypanel = Color(180,190,195),
+		dark_one = Color(150,160,165),
+	}
+end
+hook.Add("OnGamemodeLoaded", "LoadConfigAFTERTheAddon", function()
+	LoadConfig()
+end)
